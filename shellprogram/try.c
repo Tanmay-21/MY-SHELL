6d9f3941113281d10eb805de9ad0e7695d2d64a3
Tanmay-21 <trathi1526@gmail.com>
2020-12-09 22:06:35 +0530

    last modified

---
+++ shellprogram/try.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main()
+{
+    char ch;
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    ch = getch();
+    printf("%c\n", ch);
+    char ch1;
+    ch1 = getch();
+    printf("%c\n", ch1);
+    endwin();
+}
